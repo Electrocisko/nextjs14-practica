a70e9ea6e1cbd9693ced6d32e2fe361cd42825d0
Francisco Zuidwijk <electrocisko@gmail.com>
2024-06-10 14:12:53 -0300

    reset

--- app/components/FormTodo.tsx
+++ app/components/FormTodo.tsx
@@ -1,20 +1,35 @@
-'use client'
-import {createTodo} from "@/app/libs/actions/todosActions"
+"use client";
+import { createTodo } from "@/app/libs/actions/todosActions";
+import { useRef } from "react";
 
 const FormTodo = () => {
+  const formRef = useRef<HTMLFormElement>(null);
 
-    const handleSubmit = async (data: FormData) => {
-        const title = data.get("title") as string
-        await createTodo(title)
+  const handleSubmit = async (data: FormData) => {
+    const title = data.get("title") as string;
+    if (!title || !title.trim()) {
+      alert("Campo vacio");
     }
-
+    await createTodo(title);
+    formRef.current?.reset();
+  };
 
   return (
-    <form action={handleSubmit}>
-    <input type="text" name="title" id=""  placeholder="Ingrese Tarea" className="border rounded border-gray-400 mr-2 p-1"/>
-    <button type="submit" className="border rounded border-gray-400 mr-2 p-1">Agregar</button>
-</form>
-  )
-}
+    <form 
+    ref={formRef}
+    action={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        id=""
+        placeholder="Ingrese Tarea"
+        className="border rounded border-gray-400 mr-2 p-1"
+      />
+      <button type="submit" className="border rounded border-gray-400 mr-2 p-1">
+        Agregar
+      </button>
+    </form>
+  );
+};
 
-export default FormTodo
+export default FormTodo;
